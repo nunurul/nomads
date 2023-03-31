202d859d7b6167a8b438617f37324c7d498af340
khoiriyahnurul <khoiriyahnurul@apps.ipb.ac.id>
2023-03-31 15:59:18 +0700

    nomads

---
+++ resources/views/includes/style.blade.php
@@ -0,0 +1,3 @@
+<link rel="stylesheet" href="{{ url('frontend/libraries/bootstrap/css/bootstrap.css') }}">
+<link href="https://fonts.googleapis.com/css2?family=League+Spartan:wght@300;400;500;600;700&family=Playfair+Display:wght@400;500;600;700&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="{{ url('frontend/styles/main.css') }}">
---
+++ resources/views/pages/success.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.checkout')
+@section('title', 'Checkout')
+    
+@section('content')
+<main>
+    <div class="section-success d-flex align-items-center">
+        <div class="col text-center">
+            <img src="{{ url('frontend/images/ic_email.png') }}" alt="">
+            <h1>Yay! Success</h1>
+            <p>
+                We've sent you email for trip instruction
+                <br>
+                please read it as well
+            </p>
+            <a href="{{ url('/') }}" class="btn btn-home-page mt-3 px-5">Home Page</a>
+        </div>
+    </div>
+</main>
+
+@endsection
